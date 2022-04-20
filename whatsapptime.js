4a7ae7bd1b466175e0120ba2f1dbac21b8ddbfbb
MathijsNL <mathijsvandervegt@gmail.com>
2022-04-20 20:09:36 +0200

    Fix formatting

--- whatsapptime.js
+++ whatsapptime.js
@@ -2,25 +2,23 @@ function conv12to24(str) {
 	const re = /^(\d\d?):(\d\d) ([aApP])[mM]$/
 	var hr = parseInt(str.match(re)[1])
 	const mi = parseInt(str.match(re)[2])
-	if (hr == 12 && str.match(re)[3].toLowerCase() === "a") {hr = 0}
+	if (hr == 12 && str.match(re)[3].toLowerCase() === "a") { hr = 0 }
 	const add = str.match(re)[3].toLowerCase() === "p" && hr != 12
 
-
 	return (hr + 12 * add).toString().padStart(2, "0") + ":" + mi.toString().padStart(2, "0")
-
 }
 
 function lastseentodayconv12to24(str) {
-
 	const timepart = str.substring(19)
 	const convtime = conv12to24(timepart)
-	return ("last seen today at " + convtime)
 
+	return ("last seen today at " + convtime)
 }
 
 function lastseenyesterdayconv12to24(str) {
 	const timepart = str.substring(23)
 	const convtime = conv12to24(timepart)
+
 	return ("last seen yesterday at " + convtime)
 }
 
